486b21be3035055f0845ba95ed535c32a8905465
master-vk <tennis.voshod@mail.ru>
2016-01-20 00:15:20 +0200

    JS Advanced Home 1
    Task 1
    Task 2

---
+++ ITVDN_JS_Advanced/JS_Advanced_Home_1/JS_Task1.js
@@ -0,0 +1,24 @@
+﻿function Point(x, y) {
+    this.x = x;
+    this.y = y;
+
+    this.PrintCurrent = function () {
+        document.write("Exemp " + x + ", " + y + "<br />");
+    }
+}
+
+Point.Name = "Point ctor func";
+
+Point.PrintName = function () {
+    document.write(Point.Name);
+}
+
+
+
+var point1 = new Point(10, 20);
+var point2 = new Point(30, 26);
+
+point1.PrintCurrent();
+point2.PrintCurrent();
+
+Point.PrintName();
