e22d52b48b4bb7687a63f6ca5f7363f73c86a515
master-vk <tennis.voshod@mail.ru>
2016-01-24 01:51:29 +0200

    JS Advanced Home 4
    Task 1
    Task 2

---
+++ ITVDN_JS_Advanced/JS_Advanced_Home_4/JavaScript.js
@@ -0,0 +1,43 @@
+﻿var login;
+var password;
+var helper;
+var btn;
+var formLogin;
+var formPassword;
+var checkBox;
+
+window.onload = function () {
+    login = document.getElementById("login");
+    password = document.getElementById("password");
+    helper = document.getElementById("helper");
+    btn = document.getElementById("btn");
+    formLogin = document.getElementById("formLogin");
+    formPassword = document.getElementById("formPassword");
+    checkBox = document.getElementById("checkBox");
+    btn.onclick = submit;
+}
+
+function submit() {
+    if (login.value == "" || password.value == "") {
+        helper.innerText = "Вы не заполнили поля логин и пароль";
+        helper.style.color = "red";
+        formLogin.className = "has-error";
+        formPassword.className = "has-error";
+        return false;
+    }
+    if (login.value == "admin" && password.value == "12345") {
+        helper.innerText = "Вы авторизированы";
+        formLogin.className = "has-success";
+        formPassword.className = "has-success";
+        helper.style.color = "green";
+        return false;// change on true and redirect 
+    }
+    else {
+        helper.innerText = "Не верный логин или пароль";
+        helper.style.color = "orange";
+        formLogin.className = "has-warning";
+        formPassword.className = "has-warning";
+        return false;
+    }
+    
+}
