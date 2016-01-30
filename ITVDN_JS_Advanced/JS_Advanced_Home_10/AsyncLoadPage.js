a5bb14e2b85a84fc58a3753c7e591dff2590dfe8
master-vk <tennis.voshod@mail.ru>
2016-01-30 15:02:10 +0200

    JS Advanced Home 10
    Task Addition

---
+++ ITVDN_JS_Advanced/JS_Advanced_Home_10/AsyncLoadPage.js
@@ -0,0 +1,33 @@
+﻿var btn1;
+var output1;
+var output2;
+
+window.onload = function () {
+    btn1 = document.getElementById("btn1");
+    output1 = document.getElementById("output1");
+    output2 = document.getElementById("output2");
+
+    btn1.addEventListener("click", function () {
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", "PageA.html");
+
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                output1.innerHTML += xhr.responseText;
+            }
+        }
+        xhr.send();
+    });
+
+    btn1.addEventListener("click", function () {
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", "PageB.html");
+
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                output1.innerHTML += xhr.responseText;
+            }
+        }
+        xhr.send();
+    });
+}
